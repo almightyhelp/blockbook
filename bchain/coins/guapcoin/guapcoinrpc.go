b903893fdf1abcf06fc67cf29ed6a4f65aca1970
almightyhelp <61618690+almightyhelp@users.noreply.github.com>
2021-03-24 17:58:58 +0530

    Update guapcoinrpc.go

--- bchain/coins/guapcoin/guapcoinrpc.go
+++ bchain/coins/guapcoin/guapcoinrpc.go
@@ -11,7 +11,6 @@ import (
 // guapcoinRPC is an interface to JSON-RPC bitcoind service.
 type guapcoinRPC struct {
 	*btc.BitcoinRPC
-	//BitcoinGetChainInfo func() (*bchain.ChainInfo, error)
 }
 
 // NewguapcoinRPC returns new guapcoinRPC instance.
@@ -23,7 +22,6 @@ func NewguapcoinRPC(config json.RawMessage, pushHandler func(bchain.Notification
 
 	s := &guapcoinRPC{
 		b.(*btc.BitcoinRPC),
-		 //b.GetChainInfo,
 	}
 	s.RPCMarshaler = btc.JSONMarshalerV1{}
 	s.ChainConfig.SupportsEstimateFee = true
@@ -58,121 +56,4 @@ func (b *guapcoinRPC) Initialize() error {
 	glog.Info("rpc: block chain ", params.Name)
 
 	return nil
-}
-
-
-// GetBlock returns block with given hash.
-func (z *guapcoinRPC) GetBlock(hash string, height uint32) (*bchain.Block, error) {
-    var err error
-    if hash == "" && height > 0 {
-        hash, err = z.GetBlockHash(height)
-        if err != nil {
-            return nil, err
-        }
-    }
-
-    glog.V(1).Info("rpc: getblock (verbosity=1) ", hash)
-
-    res := btc.ResGetBlockThin{}
-    req := btc.CmdGetBlock{Method: "getblock"}
-    req.Params.BlockHash = hash
-    req.Params.Verbosity = 1
-    err = z.Call(&req, &res)
-
-    if err != nil {
-        return nil, errors.Annotatef(err, "hash %v", hash)
-    }
-    if res.Error != nil {
-        return nil, errors.Annotatef(res.Error, "hash %v", hash)
-    }
-
-    txs := make([]bchain.Tx, 0, len(res.Result.Txids))
-    for _, txid := range res.Result.Txids {
-        tx, err := z.GetTransaction(txid)
-        if err != nil {
-            if err == bchain.ErrTxNotFound {
-                glog.Errorf("rpc: getblock: skipping transanction in block %s due error: %s", hash, err)
-                continue
-            }
-            return nil, err
-        }
-        txs = append(txs, *tx)
-    }
-    block := &bchain.Block{
-        BlockHeader: res.Result.BlockHeader,
-        Txs:         txs,
-    }
-    return block, nil
-}
-
-
-// getinfo
-
-type CmdGetInfo struct {
-    Method string `json:"method"`
-}
-
-type ResGetInfo struct {
-    Error  *bchain.RPCError `json:"error"`
-    Result struct {
-        TransparentSupply   json.Number `json:"transparentsupply"`
-        ShieldSupply   json.Number `json:"shieldsupply"`
-        MoneySupply   json.Number `json:"moneysupply"`
-    } `json:"result"`
-}
-
-// getmasternodecount
-
-type CmdGetMasternodeCount struct {
-    Method string `json:"method"`
-}
-
-type ResGetMasternodeCount struct {
-    Error  *bchain.RPCError `json:"error"`
-    Result struct {
-        Total   int    `json:"total"`
-        Stable   int    `json:"stable"`
-        Enabled   int    `json:"enabled"`
-        InQueue   int    `json:"inqueue"`
-    } `json:"result"`
-}
-
-// GetNextSuperBlock returns the next superblock height after nHeight
-
-
-// GetChainInfo returns information about the connected backend
-// guapcoin adds Money Supply to btc implementation
-
-// findserial
-type CmdFindSerial struct {
-    Method string   `json:"method"`
-    Params []string `json:"params"`
-}
-
-type ResFindSerial struct {
-    Error  *bchain.RPCError `json:"error"`
-    Result struct {
-        Success bool      `json:"success"`
-        Txid    string    `json:"txid"`
-    } `json:"result"`
-}
-
-func (b *guapcoinRPC) Findzcserial(serialHex string) (string, error) {
-    glog.V(1).Info("rpc: findserial")
-
-    res := ResFindSerial{}
-    req := CmdFindSerial{Method: "findserial"}
-    req.Params = []string{serialHex}
-    err := b.Call(&req, &res)
-
-    if err != nil {
-        return "", err
-    }
-    if res.Error != nil {
-        return "", res.Error
-    }
-    if !res.Result.Success {
-        return "Serial not found in blockchain", nil
-    }
-    return res.Result.Txid, nil
 }
